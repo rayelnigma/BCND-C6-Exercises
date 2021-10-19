cb3b340e89be92a71d593a414fbdd2400c4922cb
Jeromy Cannon <JeromyLewisCannon@gmail.com>
2021-10-19 16:13:05 -0500

    set compiler, ganache, &openzeppelin vrs 2.3.0

--- truffle-config.js
+++ truffle-config.js
@@ -1,19 +1,31 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+
+// Be sure to match this mnemonic with that in Ganache!
+var mnemonic = "august apple property canoe anxiety weird concert identify vague kiss milk rocket";
 
 module.exports = {
   networks: {
     development: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 10);
+      provider: function () {
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/", 0, 10);
       },
-      network_id: '*',
-      gas: 9999999
+      network_id: '*'
+      // ,
+      // gas: 9999999
     }
   },
   compilers: {
     solc: {
-      version: "^0.4.25"
+      version: "0.5.16",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
     }
   }
+
 };
