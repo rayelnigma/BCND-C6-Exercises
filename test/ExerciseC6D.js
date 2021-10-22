9431fa08dce2d9704005f4d7226fd8b87bb19206
Jeromy Cannon <JeromyLewisCannon@gmail.com>
2021-10-22 14:22:44 -0500

    formatter

--- test/ExerciseC6D.js
+++ test/ExerciseC6D.js
@@ -1,4 +1,3 @@
-
 var Test = require('../config/testConfig.js');
 //var BigNumber = require('bignumber.js');
 
@@ -27,18 +26,18 @@ contract('ExerciseC6D', async (accounts) => {
 
 
   it('can register oracles', async () => {
-    
+
     // ARRANGE
     let fee = await config.exerciseC6D.REGISTRATION_FEE.call();
 
     // ACT
-    for(let a=1; a<TEST_ORACLES_COUNT; a++) {      
+    for (let a = 1; a < TEST_ORACLES_COUNT; a++) {
       await config.exerciseC6D.registerOracle({ from: accounts[a], value: fee });
     }
   });
 
   it('can request flight status', async () => {
-    
+
     // ARRANGE
     let flight = 'ND1309'; // Course number
     let timestamp = Math.floor(Date.now() / 1000);
@@ -52,14 +51,14 @@ contract('ExerciseC6D', async (accounts) => {
     // loop through all the accounts and for each account, all its Indexes (indices?)
     // and submit a response. The contract will reject a submission if it was
     // not requested so while sub-optimal, it's a good test of that feature
-    for(let a=1; a<TEST_ORACLES_COUNT; a++) {
+    for (let a = 1; a < TEST_ORACLES_COUNT; a++) {
 
       // Get oracle information
       // For a real contract, we would not want to have this capability
       // so oracles can remain secret (at least to the extent one doesn't look
       // in the blockchain data)
       let oracleIndexes = await config.exerciseC6D.getOracle(accounts[a]);
-      for(let idx=0;idx<3;idx++) {
+      for (let idx = 0; idx < 3; idx++) {
 
         try {
           // Submit a response...it will only be accepted if there is an Index match
@@ -71,7 +70,7 @@ contract('ExerciseC6D', async (accounts) => {
           //let flightStatus = await config.exerciseC6D.viewFlightStatus(flight, timestamp);
           //console.log('\nPost', idx, oracleIndexes[idx].toNumber(), flight, timestamp, flightStatus);
         }
-        catch(e) {
+        catch (e) {
           // Enable this when debugging
           // console.log('\nError', idx, oracleIndexes[idx].toNumber(), flight, timestamp);
         }
@@ -83,5 +82,5 @@ contract('ExerciseC6D', async (accounts) => {
   });
 
 
- 
+
 });
